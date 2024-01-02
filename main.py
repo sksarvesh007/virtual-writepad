04ea84eab70697dbdd61381244995876f1e6aed5
sksarvesh007 <bt22csd002@iiitn.ac.in>
2024-01-03 01:24:03 +0530

    -

---
+++ main.py
@@ -0,0 +1,11 @@
+import cv2
+import mediapipe as mp
+import os 
+import time 
+import numpy as np
+import handtrackingmodule as htm
+
+
+folderpath = "header"
+mylist = os.listdir(folderpath)
+print(mylist)
